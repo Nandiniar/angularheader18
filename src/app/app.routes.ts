48a6592194e3bb415f6bdd3c405ed304768b1608
nandini <nandani@contaque.com>
2025-05-12 11:26:36 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'about',
+     component:AboutComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+
+    },{
+        path:'contact',
+        component:ContactComponent
+    },{
+        path:'',
+        component:HomeComponent
+    },
+    {
+        path:'profile',
+        component:ProfileComponent
+    },
+    {
+        path:'**', // yeh doublestar means wild card means ke agar upar me se kuch bhi match na kare  toh yeh wale
+        component:PageNotFoundComponent
+    }
+    // 404(isse four o four page bolte hai) page or page not found tab show hota hai jab user wo wala route dekhna chahta hai jo hai he nhi toh uss wale case me default wala show hota hai
+];
